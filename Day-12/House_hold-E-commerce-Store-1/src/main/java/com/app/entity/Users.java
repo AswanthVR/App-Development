d78f9533272375aad32c9c94f5eaadee3c759624
Aswanth V R <104217859+AswanthVR@users.noreply.github.com>
2023-10-04 00:35:34 +0530

    Add files via upload

---
+++ Day-12/House_hold-E-commerce-Store-1/src/main/java/com/app/entity/Users.java
@@ -0,0 +1,95 @@
+package com.app.entity;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+public class Users implements UserDetails {
+	@Id
+	@GeneratedValue(strategy  = GenerationType.AUTO)
+	private int userId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNumber;
+	private String password;
+	
+
+
+
+	public Users() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	//
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority(role.name()));
+	}
+
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return email;
+	}
+
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return password;
+	}
+	
+	
+	
+	
+
+}
